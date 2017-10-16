ac2c66df8d3e60625320da3d13b34a4775fb0444
shishitengteng <shitengteng@aliyun.com>
2017-10-16 09:26:08 +0800

    联系人

--- src/page/contacts/CreateGroupChat.vue
+++ src/page/contacts/CreateGroupChat.vue
@@ -1,20 +1,155 @@
 
 <template>
-  <div class="hello">sss</div>
+  <div class="cgc">
+    <x-header
+      title="创建群聊"
+      :left-options="leftOptions"
+    >
+      <div slot="left">
+        <span v-if="step == 2 || step == 3">上一步</span>
+      </div>
+      <div slot="right">
+        <span v-if="step == 1 || step == 2">下一步</span>
+        <span v-if="step == 3">创建</span>
+      </div>
+    </x-header>
+    <flexbox>
+      <flexbox-item v-for="item, index in stepList" :key="index">
+        <div :class="['flex-demo', index+1 == step?'active':'']" >
+          <p>{{item.name}}</p>
+          <p>({{item.value}})</p>
+        </div>
+      </flexbox-item>
+    </flexbox>
+    <hr>
+    <div class="step_one" v-if="step == 1">
+      <h2>选择群类型：</h2>
+      <select class="chat_type">
+        <option>好友</option>
+        <option>同事</option>
+      </select>
+      <select class="chat_type">
+        <option>武汉新华科技有…</option>
+        <option>武汉新华科技有…</option>
+      </select>
+    </div>
+    <div class="step_two" v-if="step == 2">
+      <div class="group_list">
+        <span>选择群成员:</span>
+        <div class="fr" >
+          <div style="width: 1111px">
+            <img slot="icon"  src="../../assets/news/userImg.jpg" v-for="i in 11">
+          </div>
+        </div>
+      </div>
+      <contact-list type="Friends" select="true"></contact-list>
+    </div>
+    <div class="step_three" v-if="step == 3">
+      <label for="groupName">群名称：</label>
+      <input type="text" id="groupName" placeholder="请输入群名称">
+    </div>
+  </div>
 </template>
 
 <script>
+  import { XHeader, Flexbox, FlexboxItem, Divider, Selector, Group } from 'vux'
+  import contactList from './contactList'
   export default {
     name: 'hello',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        stepList: [
+          {name: '第一步', value: '选择群类型'},
+          {name: '第二步', value: '选择群成员'},
+          {name: '第三步', value: '编辑群名称'}
+        ],
+        step: 3,
+        list: [{key: 'gd', value: '好友'}, {key: 'gx', value: '同事'}],
+        value2: 'gd'
+      }
+    },
+    components: {
+      contactList,
+      XHeader,
+      Flexbox,
+      FlexboxItem,
+      Divider,
+      Selector,
+      Group
+    },
+    computed: {
+      leftOptions () {
+        if (this.step === 1) {
+          return {showBack: true}
+        } else {
+          return {showBack: false}
+        }
       }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
+  .cgc{
+    font-size: 14px;
+    min-height: 100%;
+    background-color: white!important;
+    .flex-demo {
+      padding-top: 4px;
+      text-align: center;
+      color: #A1A1A5;
+      background-clip: padding-box;
+    }
+    hr{
+      color: #A1A1A5;
+    }
+    .active{
+      color:#4F8EE5;
+    }
+    .step_one{
+      padding: 20px;
+      select{
+       width: auto;
+       padding: 0 2%;
+       margin: 0;
+       }
+      option{
+      text-align:center;
+       }
+    .chat_type{
+      margin-top: 10px;
+      margin-right: 8px;
+      font-size: 14px;
+      border:1px solid #D9D9D9;
+      padding: 4px;
+      background-color: #F3F3F3;
+      }
+    }
+    .step_two{
+      .group_list{
+        padding: 10px;
+        .fr{
+          width: 78%;
+          overflow: auto;
+        }
+        img{
+          width: 30px;
+          display:inline-block;
+          padding-right:2px;
+        }
+      }
+    }
+    .step_three{
+      padding: 10px 20px;
+      input{
+        background-color: #F1F1F1;
+        outline: 0;
+        border: 0;
+        padding: 4px;
+        border-radius: 4px;
+      }
+    }
+  }
 
 </style>
